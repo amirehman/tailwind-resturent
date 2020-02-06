5a026b003656be8d7c34724cea6a1e5ddf01bd83
Amir <aamirnorth@gmail.com>
2020-02-06 18:09:36 +0400

    update 2

--- assets/js/script.js
+++ assets/js/script.js
@@ -24,13 +24,24 @@ jQuery(document).ready(function() {
 
 
     // ---------------------- //
-
-    $('#homeCaousel').slick({
-        dots: true,
-        infinite: true,
-        speed: 500,
-        fade: true,
-        cssEase: 'linear'
-    });
+    var mySwiper = new Swiper('.swiper-container', {
+        slidesPerView: 'auto',
+        effect: 'fade',
+        loop: true,
+        fadeEffect: {
+            crossFade: true
+        },
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+            renderBullet: function(index, className) {
+                return '<span class="' + className + '">' + (index + 1) + '</span>';
+            }
+        },
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+    })
 
 })
